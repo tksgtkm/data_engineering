40c0233fd3e3e5a360f3b586a6a1d7c74870f809
tksgtkm <h9cd342fd7jgx36@gmail.com>
2026-05-01 23:28:47 +0900

    データセット生成の実装

---
+++ data_generator_platform/data_generator/datasets/devices/model.py
@@ -0,0 +1,16 @@
+import dataclasses
+from typing import Dict, Any
+
+from data_generator.datasets.data_generator_entity import DataGeneratorEntity
+
+@dataclasses.dataclass
+class Device(DataGeneratorEntity):
+    type: str
+    full_name: str
+    version: str
+
+    def as_dict(self) -> Dict[str, Any]:
+        return dataclasses.asdict(self)
+    
+    def entity_partition_key(self) -> str:
+        return self.type
