4822335416d261696d23f686e96e50f78b0d780e
tksgtkm <h9cd342fd7jgx36@gmail.com>
2026-05-02 12:20:55 +0900

    visitsデータ生成の実装(WIP)

---
+++ data_generator_platform/data_generator/io/converters.py
@@ -0,0 +1,11 @@
+import datetime
+from typing import Any
+
+def json_converter(field: Any) -> str:
+    if isinstance(field, datetime.datetime):
+        return datetime_to_str(field)
+    else:
+        return field
+
+def datetime_to_str(field: datetime.datetime) -> str:
+    return field.isoformat()
