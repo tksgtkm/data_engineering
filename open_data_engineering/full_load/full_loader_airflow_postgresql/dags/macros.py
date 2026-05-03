5145f7d8385657445bf93b5285d60256829a6052
tksgtkm <h9cd342fd7jgx36@gmail.com>
2026-05-03 16:54:47 +0900

    airflowを用いたパイプラインの構築

---
+++ open_data_engineering/full_load/full_loader_airflow_postgresql/dags/macros.py
@@ -0,0 +1,5 @@
+def get_table_name(ds_nodash: str) -> str:
+    return 'devices_' + ds_nodash
+
+def get_input_csv_to_load_for_host() -> str:
+    return '/tmp/dedp/full-loader/data-exposition/input/dataset.csv'
---
+++ open_data_engineering/full_load/full_loader_spark/python/config.py
@@ -0,0 +1,4 @@
+class DemoConfigration:
+    BASE_DIR: str = '/tmp/dedp/full-loader/'
+    INPUT_PATH: str = f'{BASE_DIR}/input'
+    DEVICES_TABLE: str = f'{BASE_DIR}/devices-table'
