c08d1bcf270f59373a21629bbeb58617779a272e
tksgtkm <h9cd342fd7jgx36@gmail.com>
2026-08-04 23:54:22 +0900

    index segment test実装

--- distributed_system/dist_system_go/log_package/internal/log/index.go
+++ distributed_system/dist_system_go/log_package/internal/log/index.go
@@ -68,7 +68,7 @@ func (i *index) Read(in int64) (out uint32, pos uint64, err error) {
 		return 0, 0, io.EOF
 	}
 	out = enc.Uint32(i.mmap[pos : pos+offWidth])
-	pos = enc.Uint64(i.mmap[pos+offWidth : pos*entWidth])
+	pos = enc.Uint64(i.mmap[pos+offWidth : pos+entWidth])
 	return out, pos, nil
 }
 
